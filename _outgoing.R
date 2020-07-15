7f6c46ba226d445962c69da4b8f2c968c4ef5131
Alex Knudson <aknudson@nevada.unr.edu>
2020-07-15 09:40:19 -0700

    updated pages according to outline. added bib file

--- _outgoing.R
+++ _outgoing.R
@@ -1,5 +1,5 @@
 # Remove all objects
-# rm(list = ls(all.names = TRUE))
+rm(list = ls(all.names = TRUE))
 
 # Unload all packages
-# pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
+pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
